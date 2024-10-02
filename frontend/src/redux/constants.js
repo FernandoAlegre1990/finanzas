eb839cdfe00fee20d4c85e1b8f5870c84d288375
Fernando Ezequiel Alegre <fernandoalegre31@gmail.com>
2024-10-02 15:21:01 +0200

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://finanzas-backend-37iu.onrender.com";
 export const USERS_URL = "/api/users";
 export const INCOMES_URL = "/api/transactions";
 export const EXPENSES_URL = "/api/transactions";
